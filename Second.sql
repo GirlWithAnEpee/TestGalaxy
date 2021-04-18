e6d721b7cec95dce7b39fc34faebd0fe1d0589a1
GirlWithAnEpee <olyuska_st@mail.ru>
2021-04-18 16:38:20 +0700

    added second sql

--- Second.sql
+++ Second.sql
@@ -2,14 +2,18 @@ SELECT
   communities.id,
   communities.name,
   permissions.name,
-  COUNT(community_members.user_id) AS expr1
-FROM communities,
-     community_member_permissions
-       INNER JOIN community_members
-         ON community_member_permissions.member_id = community_members.user_id
-       INNER JOIN permissions
-         ON community_member_permissions.permission_id = permissions.id
+  COUNT(community_members.user_id) AS AmountWhoCan
+FROM community_members
+  INNER JOIN communities
+    ON community_members.community_id = communities.id
+  INNER JOIN community_member_permissions
+    ON community_members.user_id = community_member_permissions.member_id
+  INNER JOIN permissions
+    ON community_member_permissions.permission_id = permissions.id
 GROUP BY communities.id,
          communities.name,
-         permissions.name
-ORDER BY communities.id DESC, expr1
+         permissions.name,
+         community_members.community_id,
+         permissions.id
+HAVING AmountWhoCan >= 5
+ORDER BY communities.id DESC, AmountWhoCan
